8f8922efa4451fb30db38b752fd11e5b71f8a636
Mehdi Jelassia <mjelassia@arfima.com>
2023-07-12 16:54:55 +0200

    1:34:24

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from '../model/customer.model';
+import { CustomerService } from '../services/customer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-customer',
@@ -10,17 +13,29 @@ export class NewCustomerComponent implements OnInit {
 
   newCustomerFormGroup!: FormGroup;
 
-  constructor(private fb : FormBuilder){}
+  constructor(private fb : FormBuilder, private customerService:CustomerService, private router:Router){}
 
   ngOnInit(): void {
     this.newCustomerFormGroup = this.fb.group({
-      name : this.fb.control(null),
-      email : this.fb.control(null)
+      name : this.fb.control(null, [Validators.required, Validators.minLength(4)]),
+      email : this.fb.control(null,[Validators.email,Validators.required])
 
     })
   }
   handleSaveCustomer(){
-    
+    let customer:Customer = this.newCustomerFormGroup.value
+    this.customerService.saveCustomer(customer).subscribe({
+      next : data => {
+        alert("Customer has been successfully saved !")
+        //this.newCustomerFormGroup.reset();
+        this.router.navigateByUrl("/customers")
+      },
+      error : err => {
+        console.log(err);
+        
+      }
+    })
+
   }
 
 }
